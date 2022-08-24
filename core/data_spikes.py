966e499517631cc3653c4141f135ef752ed30eb0
aaoun <andrew_aoun@brown.edu>
2022-08-24 16:18:02 -0400

    added converting between timestamp/binary data

--- core/data_spikes.py
+++ core/data_spikes.py
@@ -1,67 +1,69 @@
 
-
+# @dataclass
 class SpikeTrain():
-    """Sorted spike trains
+    """Single spike train
     """
-    @abstractmethod
-    def __init__(self, spike_ids, spike_times, spike_features):
-        pass
+    spikes_raw: list[int]
+    spike_times: list[float]
+    spike_ids: list[int]
+    spike_features: list[float]
 
-    # check if the spike train is in time-stamp format or binary format
-    # if in binary format, create binary data object and extract timestamp format as well
-    # if in time-stamp format, create time-stamp data object only
-    # have method for storing unbinned and binned rate estimates
+    def __init__(self,  timestamps, spike_features=[], spikes_raw=[], spike_times=[]):
+        self.timestamps = timestamps
 
+        assert ((len(spikes_raw) > 0) and (len(spike_times) > 0)) != True, "No spike data provided"
 
-class SpatialSpikeTrain2D(TimeStampSpikeTrain):
-    def __init__():
-        pass
+        spike_ids = []
 
+        # binary <-- time stamps
+        if len(spikes_raw) == 0:
+            spikes_raw = []
 
-class Spike():
-    """Spike
-    """
-    def __init__(self, spike_id, spike_time, spike_feature):
-        pass
+            for i in range(len(self.timestamps)):
+                if self.timestamps[i] in spike_times:
+                    spike_ids.append(i)
+                    spikes_raw.append(1)
+                else:
+                    spikes_raw.append(0)
 
+        # binary --> time stamps
+        if len(spike_times) == 0:
+            spike_times = []
 
-@dataclass
-class SpikeTrain():
-    """Spike train
-    """
-    spike_ids: List[int]
-    spike_times: List[str]
-    spike_features: List[float]
+            for i in range(len(self.timestamps)):
+                if spikes_raw[i] == 1:
+                    spike_ids.append(i)
+                    spike_times.append(self.timestamps[i])
 
-    def __init__(self, spike_ids, spike_times, spike_features):
-        self.spike_ids = spike_ids
         self.spike_times = spike_times
+        self.spikes_raw = spikes_raw
+        self.spike_ids = spike_ids
         self.spike_features = spike_features
 
     def __len__(self):
         return len(self.spike_ids)
 
     def __getitem__(self, key):
-        return self.spike_ids[key], self.spike_times[key], self.spike_features[key]
+        return self.spike_ids[key], self.spike_times[key], self.spikes_raw[key]
 
     def __iter__(self):
         for i in range(len(self.spike_ids)):
-            yield self.spike_ids[i], self.spike_times[i], self.spike_features[i]
+            yield self.spike_ids[i], self.spike_times[i], self.spikes_raw[i]
 
     def __repr__(self):
-        return f'SpikeTrain(spike_ids={self.spike_ids}, spike_times={self.spike_times}, spike_features={self.spike_features})'
+        return f'SpikeTrain(spike_ids={self.spike_ids}, spike_times={self.spike_times}, spikes_raw={self.spikes_raw})'
 
     def __str__(self):
-        return f'SpikeTrain(spike_ids={self.spike_ids}, spike_times={self.spike_times}, spike_features={self.spike_features})'
+        return f'SpikeTrain(spike_ids={self.spike_ids}, spike_times={self.spike_times}, spikes_raw={self.spikes_raw})'
 
     def __eq__(self, other):
-        return self.spike_ids == other.spike_ids and self.spike_times == other.spike_times and self.spike_features == other.spike_features
+        return self.spike_ids == other.spike_ids and self.spike_times == other.spike_times and self.spikes_raw == other.spikes_raw
 
     def __ne__(self, other):
         return not self == other
 
     def __hash__(self):
-        return hash((self.spike_ids, self.spike_times, self.spike_features))
+        return hash((self.spike_ids, self.spike_times, self.spikes_raw))
 
     def __lt__(self, other):
         return self.spike_times < other.spike_times
@@ -70,4 +72,38 @@ class SpikeTrain():
         return self.spike_times <= other.spike_times
 
     def __gt__(self, other):
-        return
+        pass
+
+
+    # check if the spike train is in time-stamp format or binary format
+    # if in binary format, create binary data object and extract timestamp format as well
+    # if in time-stamp format, create time-stamp data object onlys
+    # have method for storing unbinned and binned rate estimatess
+
+# class TimeStampSpikeTrain(SpikeTrain):
+#     """ Spike train as time stamps 
+#     """
+
+#     def __init(self, spike_times, spike_features):
+#         self.spike_times = spike_times
+
+# class BinarySpikeTrain(SpikeTrain):
+#     """ Spike train as binary 
+#     """
+
+#     def __init(self, spikes_raw, spike_features):
+#         self.spikes_raw = spikes_raw
+
+
+
+# class SpatialSpikeTrain2D(TimeStampSpikeTrain):
+#     def __init__():
+#         pass
+
+
+# class Spike():
+#     """Spike
+#     """
+#     def __init__(self, spike_id, spike_time, spike_feature):
+#         pass
+
