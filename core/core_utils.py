11ad037a511302cbd17e3b572377191a444880c9
aaoun <andrew_aoun@brown.edu>
2022-08-25 12:18:15 -0400

    added fxn to return time idx in seconds + tests

--- core/core_utils.py
+++ core/core_utils.py
@@ -1,9 +1,12 @@
 from datetime import datetime, timedelta
+from random import sample
 
-def make_time_index_from_rate(start_time, sample_length, sample_rate):
+def make_hms_index_from_rate(start_time, sample_length, sample_rate):
     """
     Creates a time index for a sample of length sample_length at sample_rate
     starting at start_time.
+
+    Output is in hours, minutes, seconds (HMS)
     """
     if type(start_time) == str:
         start_time = datetime.strptime(start_time, '%H:%M:%S')
@@ -12,3 +15,18 @@ def make_time_index_from_rate(start_time, sample_length, sample_rate):
         time_index.append(time_index[-1] + timedelta(seconds=1/sample_rate))
     str_time_index = [time.strftime('%H:%M:%S.%f') for time in time_index]
     return str_time_index
+
+def make_seconds_index_from_rate(sample_length, sample_rate):
+    """
+    Same as above but output is in seconds, start_time is automatically 0
+    Can think of this as doing all times - start_time so we have 0,0.02,0.04... array etc..
+    """
+    start_time = 0
+    dt = 1/sample_rate
+
+    time = []
+
+    for i in range(sample_length):
+        time.append(i*dt)
+
+    return time
