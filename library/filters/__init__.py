d4ba8983aba67916168ad896a0e6f06a959cee67
aaoun <andrew_aoun@brown.edu>
2022-09-13 11:54:57 -0400

    fixed flattened imports

--- library/filters/__init__.py
+++ library/filters/__init__.py
@@ -1,9 +1,16 @@
-from convert_raw_ephys_to_lfp import down_sample_ephys
-from custom_cheby import custom_cheby1
-from dc_blocker_filter import dcblock
-from fast_fourier_transform import fast_fourier
-from infinite_impulse_response_filter import iirfilt, get_a_b
-from notch_filter import notch_filt
+import os
+import sys
+
+PROJECT_PATH = os.getcwd()
+sys.path.append(PROJECT_PATH)
+print(PROJECT_PATH)
+
+from library.filters.convert_raw_ephys_to_lfp import down_sample_ephys
+from library.filters.custom_cheby import custom_cheby1
+from library.filters.dc_blocker_filter import dcblock
+from library.filters.fast_fourier_transform import fast_fourier
+from library.filters.infinite_impulse_response_filter import iirfilt, get_a_b
+from library.filters.notch_filter import notch_filt
 
 __all__ = ['down_sample_ephys', 'custom_cheby1', 'dcblock', 'fast_fourier', 'iirfilt', 'get_a_b', 'notch_filt']
 
